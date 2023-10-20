900560bdd98ff76f815def27c0080e1ec4ce800e
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-10-20 23:23:47 +0530

    upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, UploadFile, HTTPException
+import cloudinary.uploader
+import cloudinary.api
+import os
+
+# Cloudinary configuration
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+app = FastAPI()
+
+print("Cloudinary configuration:"
+      f"\n\tCloud name: {os.getenv('CLOUDINARY_CLOUD_NAME')}"
+      f"\n\tAPI key: {os.getenv('CLOUDINARY_API_KEY')}"
+      f"\n\tAPI secret: {os.getenv('CLOUDINARY_API_SECRET')}")
+
+# Retrieve stickers from Cloudinary in a specific folder
+@app.get("/get_sticker/{folder}/{sticker_id}")
+def get_sticker(folder: str, sticker_id: str):
+    try:
+        sticker_url = cloudinary.api.resource(f"{folder}/{sticker_id}")["url"]
+        return {"sticker_url": sticker_url}
+    except cloudinary.exceptions.Error as e:
+        raise HTTPException(status_code=404, detail="Sticker not found")
+
+# Upload stickers to a specific folder in Cloudinary
+@app.post("/upload_sticker/{folder}/")
+async def upload_sticker(folder: str, file: UploadFile):
+    try:
+        # Upload the sticker to Cloudinary with the specified folder
+        response = cloudinary.uploader.upload(file.file, folder=folder)
+
+        # Extract the sticker's public ID from the response
+        sticker_id = response["public_id"]
+
+        return {"sticker_id": sticker_id}
+    except cloudinary.exceptions.Error as e:
+        raise HTTPException(status_code=500, detail="Sticker upload failed")
+
+# Get a range of images from a specific folder in the Cloudinary bucket
+@app.get("/get_images_range/{folder}/{start}/{end}")
+def get_images_range(folder: str, start: int, end: int):
+    try:
+        # Fetch a list of images in the specified folder within the specified range
+        images = cloudinary.api.resources(
+            type="upload",
+            prefix=folder,
+            max_results=100,  # Adjust max_results as needed
+
+        )
+        return images["resources"][start:end]
+    except cloudinary.exceptions.Error as e:
+        raise HTTPException(status_code=500, detail="Failed to retrieve images")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # To start the FastAPI server, run the following command:
+    # uvicorn your_script_name:app --reload
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "Installing dependencies..."
+pip install -r "requirements.txt"
+
+echo "Setting environment variables..."
+export CLOUDINARY_CLOUD_NAME="djpw5ipx1"
+export CLOUDINARY_API_KEY="199759849613114"
+export CLOUDINARY_API_SECRET="bppsR3BOJl0vmL3hd5YT0Frs9kk"
+
+uvicorn main:app --reload
