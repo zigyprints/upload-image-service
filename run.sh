0e4704c6d7776559f99ff88e51dcbc68c04f421c
Zigy Prints <zigyprints@gmail.com>
2023-11-10 03:09:41 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ export CLOUDINARY_CLOUD_NAME="djpw5ipx1"
 export CLOUDINARY_API_KEY="199759849613114"
 export CLOUDINARY_API_SECRET="bppsR3BOJl0vmL3hd5YT0Frs9kk"
 
-uvicorn your_web_app:app --reload --host 0.0.0.0 --port 8000
+uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
